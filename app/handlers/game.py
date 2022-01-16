6ef50ad23b6bf73a6ca0d98f2d5abaf17f8df93e
Arnaud Boudier <arn.boudier@gmail.com>
2022-01-16 08:03:58 -0500

    test

--- app/handlers/game.py
+++ app/handlers/game.py
@@ -55,9 +55,9 @@ class GameHandler(BaseHandler):
         engine.isready()
         engine.ucinewgame()
         engine.position(board)
-
-        best_move, ponder_move = engine.go()
-
+	print("Je calcule")
+        best_move, ponder_move = engine.go(movetime=1000)
+	print("Calcul fini")
         return best_move.uci()
 
     def write_json(self, board:chess.Board):
